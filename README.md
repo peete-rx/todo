f0c2ec15a04da6c801a0595e1447ba371db32d48
peete-rx <peeter.developer@gmail.com>
2021-04-22 18:15:33 +0300

    2. Update Files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ethereum DAPP
+
+## A not complete TodoLIst Application made using Ethereum Smart Contracts
+
+#
+
+### 👨🏾‍💻 Work in progress...
--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,22 @@ pragma solidity ^0.5.0;
 /** Creating the smartcontract */
 contract TodoList {
     uint public taskCount = 0;
+
+    /** Modelling a task on a TodoList */
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check out Hive.com");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount ++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
